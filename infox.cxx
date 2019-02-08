7d2b777ad57999db3931b004e43da847ffadc849
Filippo Garbo <fgarbo01@gmail.com>
2019-02-08 18:54:54 +0100

    Add files via upload

--- infox.cxx
+++ infox.cxx
@@ -9,7 +9,6 @@ int main()
 	int contatore = 0;
 	int contatore1 = 0;
 	string prenotata;
-
 	int registrazione, num_reg;
 	int conferma;
 	char nome_completo;
@@ -21,7 +20,12 @@ int main()
 		char cliente_nome[50];
 		char cliente_cognome[50];
 		bool presa = false;
-
+		struct prenotazione
+		{
+			int giorno;
+			int mese;
+			int anno;
+		} inizio, fine;
 	} cars[20];
 
 	struct cliente
@@ -30,6 +34,12 @@ int main()
 		char cognome[50];
 		char macchina[50];
 		int anno;
+		struct prenotazione
+		{
+			int giorno;
+			int mese;
+			int anno;
+		} inizio, fine;
 		bool prenotazione = false;
 	} clients[100];
 
@@ -58,148 +68,156 @@ int main()
 		{
 			cout << "\nEcco l'elenco delle auto inserite.\n"
 				 << endl;
-			for (int a = 0; a < contatore; a++)
+for (int a = 0; a < contatore; a++)
 			{
 				cout << "Numero macchina: " << a << endl;
 
 				cout << cars[a].nome
 					 << endl;
-				if (cars[a].presa == true)
-				{
-					prenotata = "Prenotata";
-					cout << prenotata << endl;
-					cout << "Cliente che ha prenotato la macchina: " << cars[a].cliente_nome << " " << cars[a].cliente_cognome << endl
-						 << endl;
-				}
-				if (cars[a].presa == false)
-				{
-					prenotata = "Libera";
-					cout << prenotata << endl;
-				}
+			if (cars[a].presa == true)
+			{
+				prenotata = "Prenotata";
+				cout << prenotata << endl;
+				cout << "Cliente che ha prenotato la macchina: " << cars[a].cliente_nome << " " << cars[a].cliente_cognome << endl
+					 << "Prenotata da " << cars[verifica].inizio.giorno << "/" << cars[verifica].inizio.mese << "/" << cars[verifica].inizio.anno << " a " << cars[verifica].fine.giorno << "/" << cars[verifica].fine.mese << "/" << cars[verifica].fine.giorno << endl
+					 << endl;
 			}
-			break;
+			if (cars[a].presa == false)
+			{
+				prenotata = "Libera";
+				cout << prenotata << endl;
+			}
+		}
+		break;
 		}
 
-		case 3:
+	case 3:
+	{
+		cout << "\nEcco l'elenco delle auto presenti nel sistema che non sono prenotate.\n"
+			 << endl;
+
+		for (int a = 0; a < contatore; a++)
 		{
-			cout << "\nEcco l'elenco delle auto presenti nel sistema che non sono prenotate.\n"
+			if (cars[a].presa == false)
+			{
+				cout << "Numero macchina " << a << endl;
+
+				cout << cars[a].nome
+					 << endl;
+				if (cars[a].presa == true)
+					prenotata = "Prenotata";
+				if (cars[a].presa == false)
+					prenotata = "Libera";
+				cout << prenotata << endl;
+			}
+		}
+		cout << "Inserisca il numero dell'auto che vuole noleggiare" << endl;
+		cin >> verifica;
+		if (cars[verifica].presa == true)
+			prenotata = "Prenotata";
+		if (cars[verifica].presa == false)
+			prenotata = "Libera";
+		cout << cars[verifica].nome << endl;
+
+		cout << "Vuole noleggiare quest'auto? \nDigitare 1 se vuole proseguie, 2 se vuole tornare al menu' princiale." << endl;
+		cin >> conferma;
+
+		if (conferma == 1)
+		{
+			cars[verifica].presa = true;
+			cout << "E' gia' registrato come nostro cliente?\nDigiti 1 se e' nostro cliente, 2 se non lo e',\nverra' reindirizzato nella pagina di registrazione\n"
 				 << endl;
 
-			for (int a = 0; a < contatore; a++)
+			cin >> registrazione;
+
+			if (registrazione == 1)
+
 			{
-				if (cars[a].presa == false)
+				for (int a = 0; a < contatore1; a++)
 				{
-					cout << "Numero macchina " << a << endl;
-
-					cout << cars[a].nome
-						 << endl;
-					if (cars[a].presa == true)
-						prenotata = "Prenotata";
-					if (cars[a].presa == false)
-						prenotata = "Libera";
-					cout << prenotata << endl;
-				}
-			}
-			cout << "Inserisca il numero dell'auto che vuole noleggiare" << endl;
-			cin >> verifica;
-			if (cars[verifica].presa == true)
-				prenotata = "Prenotata";
-			if (cars[verifica].presa == false)
-				prenotata = "Libera";
-			cout << cars[verifica].nome << endl;
-
-			cout << "Vuole noleggiare quest'auto? \nDigitare 1 se vuole proseguie, 2 se vuole tornare al menu' princiale." << endl;
-			cin >> conferma;
-
-			if (conferma == 1)
-			{
-				cars[verifica].presa = true;
-				cout << "E' gia' registrato come nostro cliente?\nDigiti 1 se e' nostro cliente, 2 se non lo e',\nverra' reindirizzato nella pagina di registrazione\n"
-					 << endl;
-
-				cin >> registrazione;
-
-				if (registrazione == 1)
-
-				{
-					for (int a = 0; a < contatore1; a++)
-					{
-						cout << "Numero cliente: " << a << endl
-							 << "Nome: " << clients[a].nome << "\nCognome: " << clients[a].cognome << "\n"
-							 << "Anno di nascita: " << clients[a].anno << endl
-							 << endl;
-					}
-					cout << "Inserisca il numero corrispondente al suo nome: " << endl;
-
-					cin >> num_reg;
-					clients[num_reg].prenotazione = true;
-
-					for (int a = 0; a < 50; a++)
-					{
-						cars[verifica].cliente_nome[a] = clients[num_reg].nome[a];
-						clients[num_reg].macchina[a] = cars[verifica].nome[a];
-
-						cars[verifica].cliente_cognome[a] = clients[num_reg].cognome[a];
-					}
-				}
-
-				if (registrazione == 2)
-
-				{
-					clients[contatore1].prenotazione = true;
-
-					cout << "Inserisca il nome" << endl;
-					cin >> clients[contatore1].nome;
-
-					cout << "Inserisca il conome" << endl;
-					cin >> clients[contatore1].cognome;
-					cout << "Inserisca l'anno di nascita" << endl;
-					cin >> clients[contatore1].anno;
-					
-					for (int a = 0; a < 50; a++)
-					{
-						cars[verifica].cliente_nome[a] = clients[contatore1].nome[a];
-
-						cars[verifica].cliente_cognome[a] = clients[contatore1].cognome[a];
-						clients[contatore1].macchina[a] = cars[verifica].nome[a];
-					}
-
-					contatore1++;
-				}
-			}
-			break;
-		}
-		case 4:
-		{
-			cout << "Inserisca il nome" << endl;
-			cin >> clients[contatore1].nome;
-
-			cout << "Inserisca il cognome" << endl;
-			cin >> clients[contatore1].cognome;
-			cout << "Inserisca l'anno di nascita" << endl;
-			cin >> clients[contatore1].anno;
-			contatore1++;
-			break;
-		}
-		case 5:
-		{
-			for (int a = 0; a < contatore1; a++)
-			{
-				cout << "Elenco clienti registrati: " << endl;
-				cout << "Numero cliente: " << a << endl
-					 << "Nome: " << clients[a].nome << "\nCognome: " << clients[a].cognome << "\n"
-					 << "Anno di nascita: " << clients[a].anno << endl
-					 << endl;
-				if (clients[contatore1].prenotazione == true)
-				{
-					cout << "Macchina prenotata: " << clients[contatore1].macchina << endl
+					cout << "Numero cliente: " << a << endl
+						 << "Nome: " << clients[a].nome << "\nCognome: " << clients[a].cognome << "\n"
+						 << "Anno di nascita: " << clients[a].anno << endl
 						 << endl;
 				}
+				cout << "Inserisca il numero corrispondente al suo nome: " << endl;
+
+				cin >> num_reg;
+				cout << "Inserisca giorno, mese e anno di inizio noleggio?" << endl;
+				cin >> cars[verifica].inizio.giorno >> cars[verifica].inizio.mese >> cars[verifica].inizio.anno;
+
+				cout << "Inserisca giorno, mese e anno di fine noleggio?" << endl;
+				cin >> cars[verifica].fine.giorno >> cars[verifica].fine.mese >> cars[verifica].fine.anno;
+
+				for (int a = 0; a < 50; a++)
+				{
+					cars[verifica].cliente_nome[a] = clients[num_reg].nome[a];
+					clients[num_reg].macchina[a] = cars[verifica].nome[a];
+
+					cars[verifica].cliente_cognome[a] = clients[num_reg].cognome[a];
+				}
 			}
-			break;
+
+			if (registrazione == 2)
+
+			{
+				clients[contatore1].prenotazione = true;
+
+				cout << "Inserisca il nome" << endl;
+				cin >> clients[contatore1].nome;
+
+				cout << "Inserisca il conome" << endl;
+				cin >> clients[contatore1].cognome;
+				cout << "Inserisca l'anno di nascita" << endl;
+				cin >> clients[contatore1].anno;
+
+				for (int a = 0; a < 50; a++)
+				{
+					cars[verifica].cliente_nome[a] = clients[contatore1].nome[a];
+
+					cars[verifica].cliente_cognome[a] = clients[contatore1].cognome[a];
+				}
+				cout << "Inserisca giorno, mese e anno di inizio noleggio?" << endl;
+				cin >> cars[verifica].inizio.giorno >> cars[verifica].inizio.mese >> cars[verifica].inizio.anno;
+
+				cout << "Inserisca giorno, mese e anno di fine noleggio?" << endl;
+				cin >> cars[verifica].fine.giorno >> cars[verifica].fine.mese >> cars[verifica].fine.anno;
+
+				contatore1++;
+			}
 		}
-		case 6:
-			break;
+		break;
+	}
+	case 4:
+	{
+		cout << "Inserisca il nome" << endl;
+		cin >> clients[contatore1].nome;
+
+		cout << "Inserisca il cognome" << endl;
+		cin >> clients[contatore1].cognome;
+		cout << "Inserisca l'anno di nascita" << endl;
+		cin >> clients[contatore1].anno;
+		contatore1++;
+		break;
+	}
+	case 5:
+	{
+		for (int a = 0; a < contatore1; a++)
+		{
+			cout << "Elenco clienti registrati: " << endl;
+			cout << "Numero cliente: " << a << endl
+				 << "Nome: " << clients[a].nome << "\nCognome: " << clients[a].cognome << "\n"
+				 << "Anno di nascita: " << clients[a].anno << endl
+				 << endl;
 		}
-	} while (menu != 6);
+		break;
+	}
+	case 6:
+		break;
+	}
+}
+while (menu != 6)
+	;
+
+
 }
